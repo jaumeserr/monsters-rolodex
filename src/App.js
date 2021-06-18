7cf23a6cb3a86a85a728e5719097822e570c5f98
Jaume Serradell <jaumeserr@gmail.com>
2021-06-18 18:12:54 +0200

    Styling App and install github pages package

--- src/App.js
+++ src/App.js
@@ -29,11 +29,12 @@ class App extends Component {
     )
     return(
       <div className="App">
-      <SearchBox
-        placeholder="search monsters"
-        handleChange={this.handleChange}
-      />
-      <CardList monsters={filteredMonsters} />
+        <h1>Monsters Rolodex</h1>  
+        <SearchBox
+            placeholder="search monsters"
+            handleChange={this.handleChange}
+          />
+          <CardList monsters={filteredMonsters} />
       </div>
     );
   }
